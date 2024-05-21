5d289d94e4aaac357ae3f9f8fb92d976a45d44ee
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-05-21 14:03:30 +0300

    Add files via upload

---
+++ Saltikov Kursovay/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saltikov_Kursovay
+{
+    /// <summary>
+    ///  Класс, представляющий книгу
+    /// </summary>
+    public class Book
+    {
+        public string UDK { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int Copies { get; set; }
+
+        public override string ToString()
+        {
+            return $"Название: {Title}, \nАвтор: {Author}, \nГод выпуска: {Year}, \nНомер УДК: {UDK}, \nКoличество экземпляров: {Copies} \n";
+        }
+    }
+}
---
+++ Saltikov Kursovay/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saltikov_Kursovay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library lb = new Library();
+            lb.Booklibrary();
+        }
+        
+    }
+}
